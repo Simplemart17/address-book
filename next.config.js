5d04caa5acfd5f4b84bf56feac863936e954be3c
Martins Aloba <aloba.martins@gmail.com>
2023-11-21 05:11:24 -0500

    add config env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const env = process.env
+
+const {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  CLIENT_TOKEN,
+  CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET
+} = env
+
+const nextConfig = {
+  publicRuntimeConfig: {
+    CLIENT_ID,
+    CLIENT_SECRET,
+    CLIENT_TOKEN,
+    CLOUDINARY_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET
+  },
+}
 
 module.exports = nextConfig
