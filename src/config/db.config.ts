cf9584359995fcaa9241a44ea8a28907ea3a3e45
Martins Aloba <aloba.martins@gmail.com>
2023-11-21 05:13:22 -0500

    add config

---
+++ src/config/db.config.ts
@@ -0,0 +1,27 @@
+const { Client } = require('cassandra-driver')
+import getConfig from 'next/config'
+
+const {
+  publicRuntimeConfig: { CLIENT_ID, CLIENT_SECRET },
+} = getConfig();
+
+const client = new Client({
+  cloud: {
+    secureConnectBundle: 'secure-connect-multi-database.zip',
+  },
+  credentials: {
+    username: CLIENT_ID,
+    password: CLIENT_SECRET,
+  },
+})
+
+const connection = async () => {
+  try {
+    await client.connect();
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.log('Error connecting to the database: ' + error);
+  }
+}
+
+export { connection, client }
