06cb93e65632b0685354ded86ae093c4d447fb86
Martins Aloba <aloba.martins@gmail.com>
2023-11-21 20:35:21 -0500

    change api methods

---
+++ src/pages/api/users/[userEmail].ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { serverApi } from '@/config/axiosInstance';
+ 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'PATCH') {
+    try {
+      const { fullName } = req.body;
+      const { userEmail } = req.query;
+
+      await serverApi.patch(`/users/${userEmail}`, { full_name: fullName });
+
+      res.status(200).json({
+        success: true,
+        message: "User updated successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ success: false, error: "Something went wrong"});
+    }
+  } else if (req.method === "GET") {
+      try {
+        const { userEmail } = req.query;
+
+        const { data } = await serverApi.get(`/users/${userEmail}`);
+
+        res.status(200).json({ success: true, data: data[0] });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, error: "Something went wrong"});
+      }
+  } else if (req.method === "DELETE") {
+    try {
+      const { userEmail } = req.query;
+
+      await serverApi.delete(`/users/${userEmail}`);
+
+      res.status(200).json({ success: true, message: "Deleted successfully" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ success: false, error: "Something went wrong"});
+    }
+  }
+}
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,27 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { client } from '@/config/db.config'
 import { v4 as uuidv4 } from 'uuid';
- 
+import { serverApi } from '@/config/axiosInstance';
+import { json } from 'stream/consumers';
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     try {
       const { email, fullName } = req.body;
       const userType = "user";
-      const userId = uuidv4()
+      const userId = uuidv4();
+      const body: any = {
+        email,
+        full_name: fullName,
+        user_type: userType,
+        user_id: userId
+      };
 
-      const postQuery = "INSERT INTO tabular.users (user_id, email, full_name, user_type) VALUES(?,?,?,?)";
-      const getQuery = "SELECT * FROM tabular.users WHERE email=?";
+      const { data } = await serverApi.get(`/users/${email}`);
 
-      const user = await client.execute(getQuery, [email]);
-
-      if (user.rows.length === 0) {
-        await client.execute(postQuery, [userId, email, fullName, userType]);
+      if (data.length === 0) {
+        await await serverApi.post("/users", JSON.stringify(body));
       }
 
       res.status(201).json({
         success: true,
         message: "User created successfully",
-        user: user.rows[0]
+        user: data[0]
       });
     } catch (error) {
       console.log(error);
@@ -29,13 +33,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
   } else if (req.method === "GET") {
       try {
-        const query = "SELECT * FROM tabular.users";
+        const { data } = await serverApi.get('/users/rows');
 
-        const users = await client.execute(query);
-
-        res.status(200).json({ success: true, data: users.rows});
+        res.status(200).json({ success: true, data });
       } catch (error) {
-        
+        console.log(error);
+        res.status(500).json({ success: false, error: "Something went wrong"});
       }
   }
 }
