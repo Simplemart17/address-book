b1bc67db9d26e86e41fa3d61bd973023de1f8472
Martins Aloba <aloba.martins@gmail.com>
2023-11-22 10:43:27 -0500

    add contact list

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -11,180 +11,104 @@ import SlideOver from './SlideOver'
 import { useFormik } from 'formik';
 import SelectInput from './SelectInput';
 import ConfirmationModal from './ConfirmationModal';
-
-const people = [
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  {
-    name: 'Jane Cooper',
-    address: '740 Bathurst street, ON',
-    type: 'Admin',
-    email: 'janecooper@example.com',
-    phone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-]
+import axios from 'axios';
+import { formatResponseObject } from '@/utils';
+import * as Yup from 'yup';
 
 export default function ContactList(): JSX.Element {
   const [open, setOpen] = useState<boolean>(false);
   const [openDeleteDialog, setOpenDeleteDialog] = useState<boolean>(false);
   const [action, setAction] = useState<string>("");
+  const [update, setUpdate] = useState<boolean>(false);
+  const [contacts, setContacts] = useState<any>([]);
+  const [singleContacts, setSingleContacts] = useState<any>({});
+  const [docId, setDocId] = useState<string>("");
+  const [validationError, setValidationError] = useState<any>({ fileError: "" });
+
+  const email = localStorage.getItem("email");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/contacts");
+      const res = formatResponseObject(data?.data)
+      setContacts(res);
+    }
+
+    fetchData();
+  }, [update]);
+
+  useEffect(() => {
+    const fetchSingleData = async () => {
+      if (docId) {
+        const { data } = await axios.get(`/api/contacts/${docId}`);
+        setSingleContacts(data?.data);
+      }
+    }
+
+    fetchSingleData();
+  }, [docId]);
 
   const {
     values,
     handleChange,
     setValues,
     setFieldValue,
-    handleSubmit
+    handleSubmit,
+    isSubmitting,
+    setSubmitting,
+    errors,
+    resetForm
   } = useFormik({
+    enableReinitialize: true,
     initialValues: {
-      fullName: "",
-      address: "",
-      contactType: "",
-      contactImage: "",
-      fileName: "No file chosen",
+      fullName: singleContacts.fullName ?? "",
+      address: singleContacts.address ?? "",
+      phone: singleContacts.phone ?? "",
+      type: singleContacts.type ?? "",
+      email: email,
 
     },
-    onSubmit: (values: any) => {
-      console.log(values)
+    validationSchema: Yup.object({
+      fullName: Yup.string().min(3, "Enter minimum of three characters").required('This field is required'),
+      address: Yup.string().min(10, "The address is too short!").required('This field is required'),
+      phone: Yup.string().min(10, "Enter a valid phone number").required('This field is required'),
+      type: Yup.string().required('This field is required'),
+    }),
+    onSubmit: async values => {
+      try {
+        let res;
+        if (action === "add") {
+          const { data } = await axios.post("/api/contacts", values);
+          res = data;
+        } else {
+          const { data } = await axios.patch(`/api/contacts/${docId}`, values);
+          res = data;
+        }
+
+        if (res.success) {
+          setUpdate(!update);
+          setOpen(false);
+          resetForm();
+          setDocId("");
+          setSingleContacts({});
+        }
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
 
-  const handleFileChange = (event: any) => {
-    const { name, files } = event.target;
+  const deleteContact = async () => {
+    setSubmitting(true);
+    await axios.delete(`/api/contacts/${docId}`);
+    
+    setUpdate(!update);
+    setOpenDeleteDialog(false);
+    setDocId("");
+    setSingleContacts({});
+    setSubmitting(false);
+  }
 
-    if (files) {
-      const regexPattern1 = /[!@#$%^&*()+=[\]{};':"\\|,<>/?]+/;
-      const regexPattern2 = /\W{2}/g;
-      const fileName = files[0].name;
-      const filesize = files[0].size;
-      if (
-        filesize > 2000000 ||
-        fileName.length > 60 ||
-        regexPattern1.test(fileName) ||
-        fileName.match(regexPattern2)
-      ) {
-        // setValidationError({
-        //   ...validationError,
-        //   companyDocumentError: `Check file! File size not more than 2MB, file name should not contain special character and not more than 60 characters`,
-        // });
-        // setTimeout(() => {
-        //   setValidationError({ companyDocumentError: '' });
-        // }, 5000);
-
-        return;
-      }
-      setValues({ ...values, [name]: files });
-      setFieldValue('fileName', fileName);
-    }
-  };
 
   return (
     <>
@@ -193,7 +117,7 @@ export default function ContactList(): JSX.Element {
         <div className="py-10 px-28">
           <div className="flex items-center">
             <h1 className="text-4xl font-extrabold mb-7">{`${'Martins'}'s Address Book`}</h1>
-            <p className="bg-gray-600 text-[#dbecff] items-center mb-7 ml-3 p-1 rounded-md text-sm font-mono font-bold"><span className="mr-1 font-bold">{people.length}</span>{people.length > 1 ? "contacts" : "contact"}</p>
+            <p className="bg-gray-600 text-[#dbecff] items-center mb-7 ml-3 p-1 rounded-md text-sm font-mono font-bold"><span className="mr-1 font-bold">{contacts.length}</span>{contacts.length > 1 ? "contacts" : "contact"}</p>
           </div>
           <p className="ml-2">Use the search bar to look for your contact by typing the name</p>
           <div className="lg:flex items-center justify-between mb-10 block">
@@ -214,25 +138,29 @@ export default function ContactList(): JSX.Element {
             </Button>
           </div>
           <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-            {people.map((person, index) => (
+            {contacts.map((person: any, index: number) => (
               <li key={index} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-[#dbecff] shadow-lg">
                 <div className="flex w-full items-center justify-between space-x-6 p-6">
                   <div className="flex-1 truncate">
                     <div className="flex items-center space-x-3">
-                      <h3 className="truncate text-sm font-medium text-gray-900">{person.name}</h3>
+                      <h3 className="truncate text-sm font-medium text-gray-900">{person.fullName}</h3>
                       <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
                         {person.type}
                       </span>
                     </div>
                     <p className="mt-1 truncate text-sm text-gray-500">{person.address}</p>
+                    <p className="mt-1 truncate text-sm text-gray-700 font-bold">{person.phone}</p>
                   </div>
-                  <ContactImage imageUrl={person.imageUrl} index={index} />
+                  <ContactImage imageUrl={person.url} index={index} />
                 </div>
                 <div>
                   <div className="-mt-px flex divide-x divide-gray-200">
                     <div className="flex w-0 flex-1 cursor-pointer">
                       <a
-                        onClick={() => setOpenDeleteDialog(true)}
+                        onClick={() => {
+                          setOpenDeleteDialog(true);
+                          setDocId(person.documentId);
+                        }}
                         className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                       >
                         <TrashIcon className="h-5 w-5 text-red-500" aria-hidden="true" />
@@ -244,6 +172,7 @@ export default function ContactList(): JSX.Element {
                         onClick={() => {
                           setOpen(true);
                           setAction("edit");
+                          setDocId(person.documentId);
                         }}
                         className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                       >
@@ -269,6 +198,8 @@ export default function ContactList(): JSX.Element {
           name="fullName"
           type="text"
           value={values.fullName}
+          error={!!errors.fullName}
+          errorMessage={errors.fullName as string}
           label="Full Name*"
           placeholder="Enter Full Name"
           onChange={handleChange}
@@ -277,37 +208,36 @@ export default function ContactList(): JSX.Element {
           name="address"
           label="Address*"
           value={values.address}
+          error={!!errors.address}
+          errorMessage={errors.address as string}
           type="text"
           placeholder="Enter Address"
           onChange={handleChange}
         />
+        <InputField
+          name="phone"
+          label="Phone Number*"
+          value={values.phone}
+          error={!!errors.phone}
+          errorMessage={errors.phone as string}
+          type="text"
+          placeholder="Enter Phone Number"
+          onChange={handleChange}
+        />
         <SelectInput
-          name="contactType"
+          name="type"
+          error={!!errors.type}
+          errorMessage={errors.type as string}
           label="Contact Type*"
           onChange={handleChange}
         />
-        <div>
-          <p className="mt-4">Select an image*</p>
-          <div className="flex items-center mb-10 h-14 rounded-md ring-1 ring-inset ring-blue-300 focus:ring-2 focus:ring-inset focus:ring-blue-500">
-            <label className="ml-2 w-32 h-10 px-4 py-2 text-base text-white rounded-md cursor-pointer bg-blue-700 focus:outline-none">
-              <input
-                type="file"
-                className="hidden"
-                accept=".jpeg, .jpg, .png"
-                name="contactImage"
-              onChange={handleFileChange}
-              />
-              Browse Files
-            </label>
-            <p className="ml-2">{values.fileName}</p>
-          </div>
-        </div>
         <Button
-          className="w-full mt-5"
+          className={`w-full mt-14 ${isSubmitting ? 'cursor-not-allowed bg-gray-400 hover:bg-gray-400' : ''}`}
           type="submit"
+          disabled={isSubmitting}
           onClick={() => handleSubmit()}
         >
-          Submit
+          {isSubmitting ? "Submitting" : "Submit"}
         </Button>
       </SlideOver>
       <ConfirmationModal
@@ -315,7 +245,8 @@ export default function ContactList(): JSX.Element {
         setOpen={setOpenDeleteDialog}
         title="Delete Contact"
         message="Are you sure you want to delete this contact"
-        buttonAction={() => console.log("this is it")}
+        buttonAction={deleteContact}
+        loading={isSubmitting}
       />
     </>
   )
