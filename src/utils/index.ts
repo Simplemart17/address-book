33d1cb850f47ffc811b1bd3c41c8e4919290ce03
Martins Aloba <aloba.martins@gmail.com>
2023-11-22 19:01:47 -0500

    add debounce function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -36,4 +36,12 @@ const randomizeImageUrl = () => {
   return IMAGE_URL[url];
 }
 
-export { formatResponseObject, randomizeImageUrl }
+const debounce = (fn: Function, ms = 300) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
+
+export { formatResponseObject, randomizeImageUrl, debounce }
