17d310ca2e921331be699ea0a5c3778ca269d03b
Martins Aloba <aloba.martins@gmail.com>
2023-11-24 22:37:52 -0500

    contacts model

---
+++ src/models/Contacts.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+export interface ContactsProps extends mongoose.Document {
+  email: string;
+  fullName: string;
+  address: string;
+  phone: string;
+  type: string;
+  url: string;
+}
+
+const ContactSchema = new mongoose.Schema<ContactsProps>({
+  email: {
+    type: String,
+    required: [true, 'Please provide a valid email address'],
+    index: true,
+    unique: true
+  },
+  fullName: {
+    type: String,
+    required: [true, "Please provide the contact's full name"],
+    maxlength: [50, "Full Name cannot be more than 50 characters"],
+  },
+  address: {
+    type: String,
+    required: [true, 'Please enter your address'],
+  },
+  phone: {
+    type: String,
+    required: [true, 'Please provide a valid phone number'],
+    unique: true
+  },
+  type: {
+    type: String,
+  },
+  url: {
+    type: String,
+  },
+})
+
+export default mongoose.models.Contact || mongoose.model<ContactsProps>('Contact', ContactSchema)
