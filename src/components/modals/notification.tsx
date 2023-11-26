75f63c6ef6678b314cd195e057c4057a54d2aeb4
Martins Aloba <aloba.martins@gmail.com>
2023-11-25 19:26:00 -0500

    bring notification modal to the top of the page

--- src/components/modals/notification.tsx
+++ src/components/modals/notification.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react'
+import { Fragment } from 'react'
 import { Transition } from '@headlessui/react'
 import { CheckCircleIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline'
 import { XMarkIcon } from '@heroicons/react/20/solid'
@@ -16,7 +16,7 @@ export default function Notification({status, message, show, setShow}: Notificat
       {/* Global notification live region, render this permanently at the end of the document */}
       <div
         aria-live="assertive"
-        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+        className="pointer-events-none z-50 fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
       >
         <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
           {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
