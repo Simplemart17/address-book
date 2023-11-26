f85c030f88cfea761721d620a5a06109cea1a38f
Martins Aloba <aloba.martins@gmail.com>
2023-11-25 22:12:33 -0500

    clean up

--- src/pages/api/v2/contacts/[docId].ts
+++ src/pages/api/v2/contacts/[docId].ts
@@ -7,7 +7,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const { docId } = req.query;
 
       const contact = await Contacts.findByIdAndUpdate(docId, req.body, { new: true, runValidators: true });
-      console.log(contact)
 
       res.status(200).json({
         success: true,
