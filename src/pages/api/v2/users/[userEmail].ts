2eb9cbcaf59798aa56b19722cc445bc4a626f4b6
Martins Aloba <aloba.martins@gmail.com>
2023-11-25 19:27:38 -0500

    switch to v2 api

--- src/pages/api/v2/users/[userEmail].ts
+++ src/pages/api/v2/users/[userEmail].ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { serverApi } from '@/config/axiosInstance';
+import prisma from '@/lib/prisma';
  
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'PATCH') {
@@ -7,7 +7,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const { fullName } = req.body;
       const { userEmail } = req.query;
 
-      await serverApi.patch(`/keyspaces/tabular/users/${userEmail}`, { full_name: fullName });
+      await prisma.users.update({
+        where: {
+          email: userEmail as string,
+        },
+        data: {
+          full_name: fullName,
+        },
+      });
 
       res.status(200).json({
         success: true,
@@ -15,29 +22,70 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       });
     } catch (error) {
       console.log(error);
-      res.status(500).json({ success: false, error: "Something went wrong"});
+      res.status(500).json({ success: false, message: "Something went wrong"});
     }
   } else if (req.method === "GET") {
       try {
         const { userEmail } = req.query;
 
-        const { data } = await serverApi.get(`/keyspaces/tabular/users/${userEmail}`);
+        const user = await prisma.users.findUnique({
+          where: {
+            email: userEmail as string
+          },
+          include: {
+            verification: true
+          }
+        });
 
-        res.status(200).json({ success: true, data: data[0] });
+        res.status(200).json({ success: true, data: user });
       } catch (error) {
         console.log(error);
-        res.status(500).json({ success: false, error: "Something went wrong"});
+        res.status(500).json({ success: false, message: "Something went wrong"});
       }
   } else if (req.method === "DELETE") {
     try {
       const { userEmail } = req.query;
 
-      await serverApi.delete(`/keyspaces/tabular/users/${userEmail}`);
+      await prisma.users.delete({
+        where: {
+          user_id: userEmail as string,
+        }
+      });
 
       res.status(200).json({ success: true, message: "Deleted successfully" });
     } catch (error) {
       console.log(error);
-      res.status(500).json({ success: false, error: "Something went wrong"});
+      res.status(500).json({ success: false, message: "Something went wrong"});
+    }
+  } else if (req.method === "PUT") {
+    try {
+      const { userEmail } = req.query;
+
+      const user = await prisma.users.findUnique({
+        where: {
+          user_id: userEmail as string
+        },
+        include: {
+          verification: true
+        }
+      });
+
+      await prisma.verification.update({
+        where: {
+          verification_id: user?.verification[0].verification_id,
+        },
+        data: {
+          status: !user?.verification[0].status,
+        },
+      });
+
+      res.status(200).json({
+        success: true,
+        message: "Action successful",
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ success: false, message: "Something went wrong"});
     }
   }
 }
